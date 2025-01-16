c5bddc86112cb64306212be1fc76125f23d4ad30
fettis <fettisabdelkader@gmail.com>
2025-01-16 14:04:49 +0100

    vertion sans le androide

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests', // Répertoire contenant vos fichiers de test
+  testDir: './tests', // Répertoire contenant  fichiers de test
   fullyParallel: true, // Permet d'exécuter les tests en parallèle
-  forbidOnly: !!process.env.CI, // Interdit l'utilisation de "test.only" en CI
+  
   retries: process.env.CI ? 2 : 0, // Réessaie 2 fois en CI
   workers: process.env.CI ? 1 : undefined, // Utilise 1 seul thread en CI, sinon automatiquement défini
-  reporter: 'html', // Génère un rapport HTML des résultats des tests
+  reporter: 'html', // Génère un rapport HTML 
   use: {
     headless: false, // exécutent en mode "headed"
-    baseURL: 'https://www.campusfrance.org/fr/user/register', // URL de base pour vos tests
+    baseURL: 'https://www.campusfrance.org/fr/user/register', 
     trace: 'on-first-retry', // Génère une trace Playwright lors du premier réessai
     screenshot: 'only-on-failure', // Capture des écrans uniquement en cas d'échec
     video: 'retain-on-failure', // Enregistre des vidéos uniquement si un test échoue
@@ -33,15 +33,6 @@ export default defineConfig({
       name: 'iPhone 14',
       use: { ...devices['iPhone 14'] }, // Profils préconfigurés pour iPhone 14
     },
-    {
-      name: 'Android (dernière version)',
-      use: {
-        viewport: { width: 360, height: 800 }, // Résolution Android standard
-        userAgent: 'Mozilla/5.0 (Linux; Android 13; Mobile; rv:102.0) Gecko/102.0 Firefox/102.0', // User-agent moderne
-        deviceScaleFactor: 3, // Facteur d'échelle pour un appareil moderne
-        isMobile: true, // Active le mode mobile
-        hasTouch: true, // Simule un écran tactile
-      },
-    },
+    
   ],
 });
